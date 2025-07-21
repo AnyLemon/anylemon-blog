f752b0554fc9bd812a82c9729e2d3730aea0eb03
AnyLemon <1252145547@qq.com>
2025-07-21 19:31:15 +0800

    fix: 完善基础配置

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,29 +1,112 @@
 import { defineConfig } from 'vitepress'
+import { set_sidebar } from '../utils/auto_create_sidebar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/anylemon-blog/',
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  title: "AnyLemon的编程世界",
+  description: "AnyLemon的编程世界，前端，学习！",
+  head: [['link', { rel: 'icon', href: '/anylemon-blog/logo.png' }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.png',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      {
+        text: '前端',
+        items: [
+          {
+            text: 'Vue',
+            items: [
+              {
+                text: 'element-ui',
+                link: '/element-ui'
+              }
+            ]
+          },
+          {
+            text: 'React',
+            items: [
+              {
+                text: '测试',
+                link: '/测试'
+              }
+            ]
+          }
+        ]
+      },
+      { text: '学习', link: '/学习/api-examples' },
+      { text: '日常', link: '/日常/markdown-examples' }
     ],
 
-    sidebar: [
+    sidebar: {
+      '/学习/': set_sidebar('/学习'),
+      '/日常/': set_sidebar('/日常'),
+    },
+
+    // aside: 'left',
+
+    outline: {
+      level: 'deep',
+      label: '文章目录'
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/AnyLemon' },
       {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
+        icon: {
+          svg: '<svg t="1752680372381" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5557" width="200" height="200"><path d="M512 1024C229.2224 1024 0 794.7776 0 512S229.2224 0 512 0s512 229.2224 512 512-229.2224 512-512 512z m259.1488-568.8832H480.4096a25.2928 25.2928 0 0 0-25.2928 25.2928l-0.0256 63.2064c0 13.952 11.3152 25.2928 25.2672 25.2928h177.024c13.9776 0 25.2928 11.3152 25.2928 25.2672v12.6464a75.8528 75.8528 0 0 1-75.8528 75.8528H366.592a25.2928 25.2928 0 0 1-25.2672-25.2928v-240.1792a75.8528 75.8528 0 0 1 75.8272-75.8528h353.9456a25.2928 25.2928 0 0 0 25.2672-25.2928l0.0768-63.2064a25.2928 25.2928 0 0 0-25.2672-25.2928H417.152a189.6192 189.6192 0 0 0-189.6192 189.6448v353.9456c0 13.9776 11.3152 25.2928 25.2928 25.2928h372.9408a170.6496 170.6496 0 0 0 170.6496-170.6496v-145.408a25.2928 25.2928 0 0 0-25.2928-25.2672z" fill="#C71D23" p-id="5558"></path></svg>'
+        },
+        link: 'https://gitee.com/anylemon'
       }
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+    footer: {
+      copyright: "Copyright © 2025-present AnyLemon"
+    },
+
+    lastUpdated: {
+      text: '最后更新于: ',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇'
+    },
+
+    darkModeSwitchLabel: '主题',
+
+    lightModeSwitchTitle: '切换到浅色模式',
+
+    darkModeSwitchTitle: '切换到深色模式',
+
+    sidebarMenuLabel: '菜单',
+
+    returnToTopLabel: '回到顶部',
+
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            displayDetails: '展示详情列表',
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            }
+          }
+        }
+      }
+    }
   }
 })
--- index.md
+++ index.md
@@ -3,23 +3,29 @@
 layout: home
 
 hero:
-  name: "My Awesome Project"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  name: "AnyLemon的编程世界"
+  text: "学习港湾"
+  tagline: 日积跬步，以至千里
+  image:
+    src: /background.png
+    alt: 背景图片
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 学习
+      link: /学习
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 日常
+      link: /日常
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - icon: 📚
+    title: 学习前端
+    details: 还是要持续学习，加油吧哥们
+  - icon: ⏳
+    title: 分秒必争
+    details: 没必要，但还是要安排好时间
+  - icon: 📜
+    title: 内卷之王
+    details: 不至于，但是不能躺平
 ---
 
